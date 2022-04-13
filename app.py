4cf0229b3be5f2bda6ff01288f6a05bbf2d4c9a1
BR1PERCENTERS <br1percenters@gmail.com>
2022-04-13 14:33:08 +0200

    Add query function

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ CSV_FILE_2 = "data/presidents-2.csv"
 JSON_FILE_1 = "data/presidents-3.json"
 JSON_FILE_2 = "data/presidents-4.json"
 
+
+def query_db(cur, query, args=(), one=False):
+    cur = cur.execute(query, args)
+    rv = cur.fetchall()
+    return (rv[0] if rv else None) if one else rv
+
+
 # If database (in case of SQLite this is a text file) doesn't exist
 if not os.path.exists(DATABASE):
     # Create text file
@@ -24,6 +31,8 @@ try:
     # Connect to database
     con = sqlite3.connect(DATABASE)
     print("Database successfully opened")
+    # Provide both index-based and case-insensitive name-based access to columns
+    con.row_factory = sqlite3.Row
     # Create cursor object
     cur = con.cursor()
 
@@ -47,26 +56,65 @@ try:
     with open(CSV_FILE_1) as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            cur.execute("INSERT INTO president (first_name, last_name) VALUES (?, ?)", (row["first_name"], row["last_name"]))
+            cur.execute(
+                "INSERT INTO president (first_name, last_name) VALUES (?, ?)", (row["first_name"], row["last_name"])
+            )
 
     # Insert data from csv file (qmark style used with executemany())
     with open(CSV_FILE_2) as csv_file:
         csv_reader = csv.reader(csv_file)
         # Get all rows of csv from csv_reader object as list of tuples
         rows = [map(tuple, csv_reader)]
-        cur.executemany("INSERT INTO president (first_name, last_name) VALUES (?, ?)", rows[1:]) # The [1:] prevents the header row (first row) from being inserted
+        cur.executemany(
+            "INSERT INTO president (first_name, last_name) VALUES (?, ?)", rows[1:]
+        )  # The [1:] prevents the header row (first row) from being inserted
 
     # Insert data from json file (qmark style)
     with open(JSON_FILE_1) as json_file:
         data = json.load(json_file)
         for row in data:
-            cur.execute("INSERT INTO president (first_name, last_name) VALUES (?, ?)", (row["first_name"], row["last_name"]))
+            cur.execute(
+                "INSERT INTO president (first_name, last_name) VALUES (?, ?)", (row["first_name"], row["last_name"])
+            )
 
     # Insert data from json file (qmark style used with executemany())
     with open(JSON_FILE_1) as json_file:
         data = json.load(json_file)
         rows = [tuple(row.values()) for row in data]
         cur.executemany("INSERT INTO president (first_name, last_name) VALUES (?, ?)", rows)
+
+    # Retrieve all matching rows
+    response = query_db(cur, "SELECT * FROM president WHERE first_name = 'James'")
+    # Print whole row
+    print("EXAMPLE 1.1")
+    for row in response:
+        print(*row)
+    # Print parts of row index-based
+    print("EXAMPLE 1.2")
+    for row in response:
+        print(row[1], row[2])
+    # Print parts of row case-insensite name-based
+    print("EXAMPLE 1.3")
+    for row in response:
+        print(row["first_name"], row["LAST_NAME"])
+
+    # Retrieve a single matching row
+    response = query_db(cur, "SELECT * FROM president WHERE first_name = 'James'", one=True)
+    # Print whole row
+    print("EXAMPLE 2.1")
+    print(*response)
+    # Print parts of row index-based
+    print("EXAMPLE 2.2")
+    print(response[1], response[2])    
+     # Print parts of row case-insensite name-based
+    print("EXAMPLE 2.3")
+    print(response["first_name"], response["LAST_NAME"])       
+
+    # Retrieve a single matching row with placerholder
+    name = "James"
+    response = query_db(cur, "SELECT * FROM president WHERE first_name = ?", (name, ) , True)
+    print("EXAMPLE 3")
+    print(*response)
 
     # Save changes
     con.commit()
