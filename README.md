c7cf7600aa233f189cbbc8a10ed22f301ed47bac
BR1PERCENTERS <br1percenters@gmail.com>
2022-04-13 12:13:12 +0200

    Add csv support

--- README.md
+++ README.md
@@ -26,10 +26,10 @@ Finally start the app with `python3 app.py`
 
 ## Tools
 
-### Database browser
+### Database Browser
 
 [DB Browser for SQLite](https://github.com/sqlitebrowser/sqlitebrowser) is a high quality, visual, open source tool to create, design, and edit database files compatible with SQLite.
 
-## Object-relational Mappers (ORMs)
+## Object-Relational Mappers (ORMs)
 
-An object-relational mapper (ORM) is a code library that automates the transfer of data stored in relational database tables into objects that are more commonly used in application code. The most famous ORM's for Python are [SQLAlquemy ORM](https://github.com/sqlalchemy/sqlalchemy), [Peewee ORM](https://github.com/coleifer/peewee) (["simpler, smaller and more hackable"](https://charlesleifer.com/blog/peewee-a-lightweight-python-orm---original-post/)) and (Django's ORM)[https://github.com/django/django] (if you use Django).
+An object-relational mapper (ORM) is a code library that automates the transfer of data stored in relational database tables into objects that are more commonly used in application code. The most famous ORM's for Python are [SQLAlquemy ORM](https://github.com/sqlalchemy/sqlalchemy), [Peewee ORM](https://github.com/coleifer/peewee) (["simpler, smaller and more hackable"](https://charlesleifer.com/blog/peewee-a-lightweight-python-orm---original-post/) than SQLAlquemy) and (Django's ORM)[https://github.com/django/django] (if you use Django).
--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import os
 import sqlite3
+import csv
+import json
 
+# Define file paths
 DATABASE = "example.db"
+CSV_FILE_1 = "data/presidents-1.csv"
+CSV_FILE_2 = "data/presidents-2.csv"
 
 # If database (in case of SQLite this is a text file) doesn't exist
 if not os.path.exists(DATABASE):
     # Create text file
     with open(DATABASE, "w"):
         pass
+# If database exists
+else:
+    # Delete database
+    os.remove(DATABASE)
 
 try:
     # Connect to database
@@ -15,13 +24,36 @@ try:
     print("Database successfully opened")
     # Create cursor object
     cur = con.cursor()
+
     # Create table
     cur.execute(
-        """CREATE TABLE stocks
-                (date text, trans text, symbol text, qty real, price real)"""
+        """CREATE TABLE president
+        (
+            id INTEGER PRIMARY KEY,
+            first_name TEXT,
+            last_name TEXT
+        )"""
     )
-    # Insert a row of data
-    cur.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+    # Insert row of data
+    cur.execute("INSERT INTO president VALUES (1, 'George', 'Washington')")
+
+    # Insert row of data without id
+    cur.execute("INSERT INTO president (first_name, last_name) VALUES ('John', 'Adams')")
+
+    # Insert data from csv file (qmark style)
+    with open(CSV_FILE_1) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            cur.execute("INSERT INTO president (first_name, last_name) VALUES (?, ?)", (row["first_name"], row["last_name"]))
+
+    # Insert data from csv file (qmark style used with executemany())
+    with open(CSV_FILE_2) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        # Get all rows of csv from csv_reader object as list of tuples
+        rows = list(map(tuple, csv_reader))
+        cur.executemany("INSERT INTO president (first_name, last_name) VALUES (?, ?)", rows[1:]) # The [1:] prevents the header row (first row) from being inserted
+
     # Save changes
     con.commit()
     print("Changes saved")
